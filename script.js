ba3ea536bd1c0c4f3d7e62674f6b1b50f1347857
Valeriy Paseka <valeriy.paseka@gmail.com>
2017-09-08 20:14:59 +0300

    speed of movement of objects

--- script.js
+++ script.js
@@ -1,137 +1,163 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+canvas.width = 600;
+canvas.height = 400;
+
+
+
 var keys = {
-	'Left' : 37,
-	'Right' : 39,
-	'Space' : 32
+  'Left' : 37,
+  'Right' : 39,
+  'Space' : 32
 };
 
 var keyDown = {};
 
 var setKey = function (keyCode) {
-	keyDown[keyCode] = true;
+  keyDown[keyCode] = true;
 };
 
 var clearKey = function (keyCode) {
-	keyDown[keyCode] = false;
+  keyDown[keyCode] = false;
 };
 
 var isKeyDown = function (keyName) {
-	return keyDown[keys[keyName]] == true;
+  return keyDown[keys[keyName]] == true;
 };
 
 
 window.onkeydown = function (e) {
-	setKey(e.keyCode);
+  setKey(e.keyCode);
 };
 
 window.onkeyup = function (e) {
-	clearKey(e.keyCode);
-};	
+  clearKey(e.keyCode);
+};
 
 
 var drawRect = function (x, y, sizeX, sizeY, color) {
-	ctx.beginPath();
-	ctx.rect(x, y, sizeX, sizeY);
-	ctx.fillStyle = color;
-	ctx.fill();
-	ctx.closePath();
+  ctx.beginPath();
+  ctx.rect(x, y, sizeX, sizeY);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.closePath();
 };
 
 var drawPlayer = function (x) {
-	ctx.beginPath();
-	ctx.rect(x, 145, 50, 5);
-	ctx.rect(x+10, 140, 30, 5);
-	ctx.rect(x+23, 135, 5, 5);
-	ctx.fillStyle = "rgb(173, 105, 82)";
-	ctx.fill();
-	ctx.closePath();
+  ctx.beginPath();
+  ctx.rect(x, 395, 50, 5);
+  ctx.rect(x+10, 390, 30, 5);
+  ctx.rect(x+23, 385, 5, 5);
+  ctx.fillStyle = "rgb(173, 105, 82)";
+  ctx.fill();
+  ctx.closePath();
 };
 
 var Destruction = function () {
-	for(var i = 0; i < shots.length; i++) {
-		for(var j = 0; j < ships.length; j++) {
-			if(~~shots[i].y == ~~ships[j].y+5 && shots[i].x+5 >= ships[j].x && shots[i].x <= ships[j].x+10) {
-				ships.splice(j, 1);
-				shots.splice(i, 1);
-				break;
-			}
-		}
-	}
+  for(var i = 0; i < shots.length; i++) {
+    for(var j = 0; j < ships.length; j++) {
+      if(~~shots[i].y == ~~ships[j].y+5 && shots[i].x+5 >= ships[j].x && shots[i].x <= ships[j].x+10) {
+        ships.splice(j, 1);
+        shots.splice(i, 1);
+        break;
+      }
+    }
+  }
+};
+;
+var shipsGeneration = function (time) {
+  if( countShips > densityShips && ships.length < numberShips) {
+    ships.push({
+      t : speedShips*1000,//время падения 
+      ts : time,//время старта
+      x : Math.random() * 590|0,
+      y : 0
+    });
+    countShips = 0;
+  }
+  countShips++;
+
+  for(var i = 0; i < ships.length; i++) {
+    var s = ships[i], p = (time-s.ts)/s.t;
+    if(p < 1)s.y = p*400|0;
+    else {s.y= 0; s.ts = time ; s.t= speedShips*1000;
+    s.x = Math.random() * 590|0;
+    };
+
+    drawRect(s.x, s.y, 10, 10, "rgb(22, 105, 67)");
+
+  }
 };
 
-var shipsGeneration = function () {
-	if( countShips > 30) {
-		ships.push({
-			x : Math.floor(Math.random() * (600 - 0 + 1)) + 0,
-			y : 0
-		});
-		countShips = 0;
-	}
-	countShips++;
+var shotsGeneration = function (frameTime) {
+  if(isKeyDown('Space')) {
+    var length = shots.length;
+    if(length > 0) {
+      if( (shots[length-1].y + 7) < 380) {
+        shots.push({
+          x : xPlayer+23,
+          y : 380
+        });
+      }
+    } else {
+      shots.push({
+        x : xPlayer+23,
+        y : 380
+      });
+    }
 
-	for(var i = 0; i < ships.length; i++) {
+  }
 
-		drawRect(ships[i].x, ships[i].y, 10, 10, "rgb(22, 105, 67)");
-		ships[i].y += .3;
-	}
+  for(var i = 0; i < shots.length; i++) {
+
+    drawRect(shots[i].x, shots[i].y, 5, 5, "rgb(195, 55, 67)");
+    shots[i].y -= (frameTime / 1000) * speedShots;
+  }
 };
 
-var shotsGeneration = function () {
-	if(isKeyDown('Space')) {
-		var length = shots.length;
-		if(length > 0) {
-			if( (shots[length-1].y + 7) < 130) {
-				shots.push({
-					x : xPlayer+23,
-					y : 130
-				});
-			}              
-		} else {
-			shots.push({
-				x : xPlayer+23,
-				y : 130
-			});
-		}
-		
-	}
-
-	for(var i = 0; i < shots.length; i++) {
-
-		drawRect(shots[i].x, shots[i].y, 5, 5, "rgb(195, 55, 67)");
-		shots[i].y -= 2;
-	}
-};
-
-var Player = function () {
-	if(isKeyDown('Left')) {
-		xPlayer-=4;
-	}
-	if(isKeyDown('Right')) {
-		xPlayer+=4;
-	}
-	drawPlayer(xPlayer);
+var Player = function (frameTime) {
+  if(isKeyDown('Left')) {
+    xPlayer -= (frameTime / 1000) * speedPlayer;
+  }
+  if(isKeyDown('Right')) {
+    xPlayer += (frameTime / 1000) * speedPlayer;
+  }
+  drawPlayer(xPlayer);
 };
 
 var ships = [];
 var shots = [];
-var countShips = 0;
+var densityShips = 0;
 var xPlayer = 120;
+var lastTime = 0;
+var countShips = 0;
 
-var main = function () {
+var speedPlayer = 200; // скорость передвижения игрока
+var densityShips = 30; // плотность кораблей
+var numberShips = 20; // количество кораблей
+var speedShips = 15;
+var speedShots = 150; // скорость пуль
+
+var main = function (time) {
 	
-	ctx.clearRect(0, 0, 600, 400);
+  ctx.clearRect(0, 0, 600, 400);
+  drawRect(0, 0, 600, 400, "rgb(36, 177, 219)");
 
-	Destruction();
+  var startTime = time;
+  var frameTime = time - lastTime;
 
-	shipsGeneration();
+  Destruction();
 
-	shotsGeneration();
-	
-	Player();
+  shipsGeneration(time);
 
-	requestAnimationFrame(main);	
+  shotsGeneration(frameTime);
+
+  Player(frameTime);
+
+  lastTime = startTime;
+
+  requestAnimationFrame(main);
 };
 
 main();
